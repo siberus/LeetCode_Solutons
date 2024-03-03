5ff170cd9df841e1143be7dce8362b9fda02c205
siberus <mr.siberus@yandex.ru>
2024-03-03 19:05:33 +0700

    final 4th task

--- MedianofTwoSortedArrays.cpp
+++ MedianofTwoSortedArrays.cpp
@@ -16,28 +16,42 @@ namespace std
             while (imin <= imax) {
                 int i = (imin + imax) / 2;
                 int j = half_len - i;
-
-
-
-            int position = 0;
-            int a, b;
-            vector<int>twonums;
-            if (len % 2 != 0)
-            {
-                position = len / 2 + 1;
-            }
-            else position = len / 2;
-
-            for (int i = 0; i < position+1; i++)
-            {   a = nums1[i];
-                b = nums2[i]
-                if (a)
-                {
-                    /* code */
+                if (i < m && nums2[j - 1] > nums1[i]) {
+                    imin = i + 1;
+                } 
+                else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                    imax = i - 1;
+                } 
+                else {
+                    int lmax = 0;
+                    // вычисляем левую часть медианы
+                    if (i == 0) {
+                        lmax = nums2[j - 1];
+                    } else if (j == 0) {
+                        lmax = nums1[i - 1];
+                    } else {
+                        lmax = max(nums1[i - 1], nums2[j - 1]);
+                    }
+                    // если сумма размеров массивов нечетная, возвращаем lmax
+                    if ((m + n) % 2 == 1) {
+                        return lmax;
+                    }
+                    int rmin = 0;
+                    // вычисляем правую часть медианы
+                    if (i == m) {
+                        rmin = nums2[j];
+                    } else if (j == n) {
+                        rmin = nums1[i];
+                    } else {
+                        rmin = min(nums1[i], nums2[j]);
+                    }
+                    // если сумма размеров массивов четная, возвращаем среднее значение
+                    return (lmax + rmin) / 2.0;
                 }
-                
             }
-            
+            // если массивы не пересекаются, возвращаем 0.0
+            return 0.0;    
         }
+        
     };
 }
